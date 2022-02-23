4881ca99a90c9a945479f2d050513e985aa6eee3
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2022-02-23 10:32:00 -0400

    paginacion de articulos

--- app/Http/Livewire/Articles.php
+++ app/Http/Livewire/Articles.php
@@ -3,11 +3,17 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
-
+use App\Models\Article;
+use Livewire\WithPagination;
 class Articles extends Component
 {
+    use WithPagination;
+
     public function render()
     {
-        return view('livewire.articles');
+        $articles = Article::where('user_id', auth()->user()->id)->paginate(10);
+        return view('livewire.articles',[
+            'articles' => $articles,
+        ]);
     }
 }
--- resources/views/livewire/articles.blade.php
+++ resources/views/livewire/articles.blade.php
@@ -1,3 +1,46 @@
-<div>
-    Viene del componente Livewire, suscribirte
+<div class="p-2 sm:px-20 bg-white border-b border-gray-200">
+    <div class="mt-4 text-2xl">
+        <div>Articulos</div>
+    </div>
+    <div class="mt-3">
+        <table class="table-auto w-full">
+            <thead>
+                <tr>
+                    <th class="px-4 py-2">
+                        <div class="flex items-center">ID</div>
+                    </th>
+                    <th class="px-4 py-2">
+                        <div class="flex items-center">Descripcion</div>
+                    </th>
+                    <th class="px-4 py-2">
+                        <div class="flex items-center">Precio</div>
+                    </th>
+                    <th class="px-4 py-2">
+                        <div class="flex items-center">Cantidad</div>
+                    </th>
+                    <th class="px-4 py-2">
+                        Status
+                    </th>
+                    <th class="px-4 py-2">
+                        Accion
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($articles as $article)
+                    <tr>
+                        <td class="rounded border px-4 py-2">{{ $article->id }}</td>
+                        <td class="rounded border px-4 py-2">{{ $article->name }}</td>
+                        <td class="rounded border px-4 py-2">{{ number_format($article->price,2) }}</td>
+                        <td class="rounded border px-4 py-2">{{ $article->quantity }}</td>
+                        <td class="rounded border px-4 py-2">{{ $article->status ? 'Activo':'Inactivo' }}</td>
+                        <td class="rounded border px-4 py-2">Editar/Eliminar</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+    <div class="mt-4">
+        {{ $articles->links() }}
+    </div>
 </div>
